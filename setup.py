24dfdc7eb779febab953304db24bd4779367ff2f
dulude <dulude@stsci.edu>
2023-07-14 13:39:30 -0400

    Added setup.py and some other stuff

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pep8_notebook_check',
+    version='1',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='dulude',
+    author_email='dulude@stsci.edu',
+    description='PEP8 style checker for python code embedded in Jupyter notebooks'
+)
